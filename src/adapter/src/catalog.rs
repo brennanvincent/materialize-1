sploiselle/materialize

storage-client: add param to disable shard finalization

src/adapter/src/catalog.rs
@@ -7343,6 +7343,7 @@ impl Catalog {
                     }
                 }
             },
+            finalize_shards: self.system_config().storage_client_finalize_shards(),
         }
     }
 

brennanvincent/materialize-1

src/adapter/src/catalog.rs
@@ -7291,6 +7291,7 @@ impl Catalog {
                     }
                 }
             },
+            finalize_shards: self.system_config().enable_storage_shard_finalization(),
         }
     }
 